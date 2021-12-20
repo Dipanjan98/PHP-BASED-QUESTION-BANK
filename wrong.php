49eebae5f519f6220d2069f9fd5fad76dce964c8
Dipanjan Swarnakar <60978356+Dipanjan98@users.noreply.github.com>
2021-12-21 01:26:32 +0600

    Add files via upload

---
+++ wrong.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<style>
+		body{
+			text-align: center;
+			margin: 20px auto;
+		}
+	</style>
+</head>
+<body>
+  <img src="working_on_sarver.gif" alt="">
+</body>
+</html>
